9ab7cabe34d62ec2d89be6bb962eccfea7b5459c
Alexei Lifanov <alexeilifanov@alexeis-air.myfiosgateway.com>
2021-07-28 16:49:02 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+dodger.style.bottom = "100px";
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    const width = 400;
+    const widthDodger = 40;
+    const maxValue = width - widthDodger;
+
+    if (left < maxValue) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
+
+document.addEventListener("keydown", function(event) {
+    if (event.key === "ArrowLeft") {
+        moveDodgerLeft()
+    } else if (event.key === "ArrowRight") {
+        moveDodgerRight()
+    }
+});
